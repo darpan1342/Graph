b646ddd1036f8d78681f168df3b7f0481e61f517
BurnedQuaterBack <36494771+darpan1342@users.noreply.github.com>
2019-01-12 01:52:37 +0530

    Update Graph.java

--- Dfs_Applications/Graph.java
+++ Dfs_Applications/Graph.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Graph {
     int v;
     LinkedList<Integer> a[];
+    int count;
 
     Graph(int v) {
         this.v = v;
@@ -10,12 +11,17 @@ class Graph {
         for (int i = 0; i < v; i++) {
             a[i] = new LinkedList<>();
         }
+        count = 0;
     }
 
     void adde(int src, int dest) {
         a[src].add(dest);
         if (src != dest)
             a[dest].add(src);
+    }
+
+    void addD(int src, int dest) {
+        a[src].add(dest);
     }
 
     void dfs(int y, boolean visited[]) {
@@ -25,6 +31,31 @@ class Graph {
                 dfs(i, visited);
             }
         }
+    }
+
+    void dfs(int y, boolean visited[], Stack<Integer> s) {
+        visited[y] = true;
+        for (int j : a[y]) {
+            if (visited[j] == false) {
+
+                dfs(j, visited, s);
+            }
+        }
+        s.push(y);
+    }
+
+    void topo() {
+        boolean[] visited = new boolean[v];
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == false)
+                dfs(i, visited, s);
+        }
+        while (!s.empty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
     }
 
 }
